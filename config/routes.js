bdd25a34de778c334fc067c83fb539221cac338a
sdickscheid <scott.dickscheid@yahoo.com>
2017-10-24 17:41:22 -0700

    without routes file

--- config/routes.js
+++
@@ -1,7 +0,0 @@
-//Update the name of the controller below and rename the file.
-const users = require("../controllers/users.js")
-module.exports = function(app){
-
-  app.get('/', template.index);
-
-}
